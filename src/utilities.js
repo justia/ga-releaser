880ba945c80d9c8dc62c451d3e1f0d3fb7df906e
aazbeltran <aazbeltran@outlook.com>
2020-11-06 18:15:07 -0600

    refactor: node script is moved to front-end-projects

--- src/utilities.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-const parseInputBoolean = (value, defaultValue) =>
-    value !== undefined && value !== '' ? value === 'true' : defaultValue;
-const isBoolean = (val) => typeof val === 'boolean';
-
-const dirname = path.resolve(__dirname);
-
-module.exports = { parseInputBoolean, dirname, isBoolean };
